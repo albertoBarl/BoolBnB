2597b25e336eb7f1f1a35806d027b48a61dcca81
albertoBarl <alberto.barletta00@gmail.com>
2023-03-22 16:06:58 +0100

    added sponsors migration

---
+++ database/migrations/2023_03_22_121425_create_sponsors_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSponsorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sponsors', function (Blueprint $table) {
+            $table->tinyIncrements('id');
+            $table->string('title');
+            $table->string('slug');
+            $table->float('price');
+            $table->tinyInteger('duration');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sponsors');
+    }
+}
