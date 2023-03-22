328b4e234dde435c87b2a85317b4791408da33da
albertoBarl <alberto.barletta00@gmail.com>
2023-03-22 16:35:58 +0100

    added Apartment -rcs --requests

---
+++ app/Http/Controllers/ApartmentController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreApartmentRequest;
+use App\Http\Requests\UpdateApartmentRequest;
+use App\Models\Apartment;
+
+class ApartmentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreApartmentRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreApartmentRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Apartment  $apartment
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Apartment $apartment)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Apartment  $apartment
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Apartment $apartment)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateApartmentRequest  $request
+     * @param  \App\Models\Apartment  $apartment
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateApartmentRequest $request, Apartment $apartment)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Apartment  $apartment
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Apartment $apartment)
+    {
+        //
+    }
+}
