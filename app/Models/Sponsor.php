524d041328fb70c9c52366b1bba797d59741d104
albertoBarl <alberto.barletta00@gmail.com>
2023-03-23 10:58:06 +0100

    sponsor added

---
+++ app/Models/Sponsor.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+
+class Sponsor extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+
+    // slug function
+    // public static function genSlug($param)
+    // {
+    //     return Str::slug($param, "-");
+    // }
+}
