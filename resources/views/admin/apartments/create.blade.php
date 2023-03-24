bb7e4560e0a15c727f4aedd436c62e7a796230be
albertoBarl <alberto.barletta00@gmail.com>
2023-03-24 11:43:56 +0100

    fixed labels

--- resources/views/admin/apartments/create.blade.php
+++ resources/views/admin/apartments/create.blade.php
@@ -5,7 +5,7 @@
         <div class="col-12">
             <h2 class="py-3">Aggiungi nuovo progetto</h2>
         </div>
-        @if($errors->any())
+        @if ($errors->any())
             <div class="alert alert-danger">
                 <ul class="list-unstyled">
                     @foreach ($errors->all() as $error)
@@ -20,7 +20,7 @@
                 <div class="form-group my-3">
                     <label class="control-label">Titolo</label>
                     <input type="text" class="form-control" placeholder="Title" id="title" name="title">
-                    @if($errors->any())
+                    @if ($errors->any())
                         @foreach ($errors->all() as $error)
                             <div class="text-danger">{{ $error }}</div>
                         @endforeach
@@ -31,61 +31,64 @@
                     <textarea class="w-100" rows="10" type="text" name="description" placeholder="Inserisci la descrizione"></textarea>
                 </div>
                 <div class="form-group my-3">
-                    <label class="control-label">Titolo</label>
-                    <input type="number" class="form-control" placeholder="Metratura stanza" id="square_feet" name="square_feet">
+                    <label class="control-label">Metri quadri</label>
+                    <input type="number" class="form-control" placeholder="Metratura stanza" id="square_feet"
+                        name="square_feet">
                 </div>
                 <div class="form-group my-3">
-                    <label class="control-label">Titolo</label>
+                    <label class="control-label">n. Bagni</label>
                     <input type="number" class="form-control" placeholder="Numero bagni" id="bathroom" name="bathroom">
                 </div>
                 <div class="form-group my-3">
-                    <label class="control-label">Titolo</label>
+                    <label class="control-label">n. Stanze</label>
                     <input type="number" class="form-control" placeholder="Numero stanze" id="room" name="room">
                 </div>
                 <div class="form-group my-3">
-                    <label class="control-label">Titolo</label>
+                    <label class="control-label">Indirizzo</label>
                     <input type="text" class="form-control" placeholder="Indirizzo" id="address" name="address">
                 </div>
                 <div class="form-group my-3">
                     <label class="control-label">Copertina</label>
-                    <input type="file" name="image" id="image" class="form-control
+                    <input type="file" name="image" id="image"
+                        class="form-control
                     @error('image')is-invalid @enderror">
                     @error('image')
-                    <div class="text-danger">
-                    @enderror
-                </div>
-                <div class="form-group my-3">
-                    <label class="control-label">Titolo</label>
-                    <input type="number" class="form-control" placeholder="Latitudine" id="latitude" name="latitude">
-                </div>
-                <div class="form-group my-3">
-                    <label class="control-label">Titolo</label>
-                    <input type="number" class="form-control" placeholder="Longitudine" id="longitude" name="longitude">
-                </div>
-                <div class="form-group my-3">
-                    <label class="control-label">Titolo</label>
-                    <input type="number" class="form-control" placeholder="Prezzo" id="price" name="price">
-                </div>
-                <div class="form-group my-3">
-                    <label class="control-label">Tipo</label>
-                    <select class="form-control" name="free_cancellation" id="free_cancellation">
-                        <option value="1">si</option>
-                        <option value="0">no</option>
-                    </select>
-                </div>
-                <div class="form-group my-3">
-                    <label class="control-label">services</label>
-                    @foreach ($services as $service)
-                        <div>
-                            <input type="checkbox" value="{{ $service->id }}" name="services[]">
-                            <label class="form-check-label">{{ $service->name }}</label>
-                        </div>
-                    @endforeach
-                </div>
-                <div class="form-group my-3">
-                    <button type="submit" class="btn btn-sm btn-success">Salva progetto</button>
-                </div>
+                        <div class="text-danger">
+                        @enderror
+                    </div>
+                    <div class="form-group my-3">
+                        <label class="control-label">Latitudine</label>
+                        <input type="number" class="form-control" placeholder="Latitudine" id="latitude" name="latitude">
+                    </div>
+                    <div class="form-group my-3">
+                        <label class="control-label">Longitudine</label>
+                        <input type="number" class="form-control" placeholder="Longitudine" id="longitude"
+                            name="longitude">
+                    </div>
+                    <div class="form-group my-3">
+                        <label class="control-label">Prezzo</label>
+                        <input type="number" class="form-control" placeholder="Prezzo" id="price" name="price">
+                    </div>
+                    <div class="form-group my-3">
+                        <label class="control-label">Cancellazione gratuita?</label>
+                        <select class="form-control" name="free_cancellation" id="free_cancellation">
+                            <option value="1">si</option>
+                            <option value="0">no</option>
+                        </select>
+                    </div>
+                    <div class="form-group my-3">
+                        <label class="control-label">Servizi</label>
+                        @foreach ($services as $service)
+                            <div>
+                                <input type="checkbox" value="{{ $service->id }}" name="services[]">
+                                <label class="form-check-label">{{ $service->name }}</label>
+                            </div>
+                        @endforeach
+                    </div>
+                    <div class="form-group my-3">
+                        <button type="submit" class="btn btn-sm btn-success">Salva progetto</button>
+                    </div>
             </form>
         </div>
     </div>
-@endsection. 
+@endsection.
