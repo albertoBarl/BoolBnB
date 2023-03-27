23a499c420c551d78e9ffe0e90750689983bdff0
leo5295 <leonardo.martellosio@gmail.com>
2023-03-27 11:48:17 +0200

    service create and store remove

--- app/Http/Controllers/admin/ServiceController.php
+++ app/Http/Controllers/admin/ServiceController.php
@@ -14,26 +14,4 @@ class ServiceController extends Controller
         $services = Service::all();
         return view('admin.services.index', compact('services'));
     }
-    public function create()
-    {
-        return view('admin.services.create');
-    }
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \App\Http\Requests\StoreApartmentRequest  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(StoreServiceRequest $request)
-    {
-        $form_data = $request->all();
-
-        $slug = Service::genSlug($request->name);
-
-        $form_data['slug'] = $slug;
-
-        Service::create($form_data);
-
-        return redirect()->route('admin.services.index');
-    }
 }
--- resources/views/admin/services/index.blade.php
+++ resources/views/admin/services/index.blade.php
@@ -1,8 +1,6 @@
 @extends('layouts.admin')
 @section('content')
-<a class="d-flex justify-content-end" href="{{route('admin.services.create')}}">
-    <button class="btn btn-primary m-3">Aggiungi nuovi servizi</button>
-</a>
+
 @foreach ($services as $item)
 <div class="m-2">
     <li>
