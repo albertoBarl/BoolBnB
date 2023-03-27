001ed086b40f52426b1d97bbf02fe87402e2ee56
mattiabenelli <benellimattia0@gmail.com>
2023-03-27 12:39:32 +0200

    show done

--- app/Http/Controllers/admin/ApartmentController.php
+++ app/Http/Controllers/admin/ApartmentController.php
@@ -73,7 +73,7 @@ class ApartmentController extends Controller
      */
     public function show(Apartment $apartment)
     {
-        //
+        return view('admin.apartments.show', compact('apartment'));
     }
 
     /**
