ba4dca8c8f7bf29e470d09996d392960d7aba463
mattiabenelli <benellimattia0@gmail.com>
2023-03-29 11:48:54 +0200

    controller e routes api

---
+++ app/Http/Controllers/Api/ApartmentController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\Apartment;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ApartmentController extends Controller
+{
+    public function index(){
+        $apartments = Apartment::with('services', 'sponsors')->paginate(8);
+        return response()->json([
+            'success' => true,
+            'results' => $apartments
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ApartmentController as ApartmentController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,8 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::get('/apartments', [ApartmentController::class, 'index']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
