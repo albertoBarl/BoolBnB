b1d982cb06ce5d4c7dc4e4ce83b18cd7d03e5ef8
albertoBarl <alberto.barletta00@gmail.com>
2023-03-30 10:37:39 +0200

    added cordinates route

--- routes/api.php
+++ routes/api.php
@@ -19,21 +19,21 @@ use App\Http\Controllers\Api\ApartmentController as ApartmentController;
 Route::get('/apartments', [ApartmentController::class, 'index']);
 
 // searchbar
-// Route::get('/geocode/{address}', function ($address) {
-//     $client = new \GuzzleHttp\Client(["verify" => false]);
-//     $response = $client->get('https://api.tomtom.com/search/2/geocode/' . urlencode($address) . '.json', [
-//         'query' => [
-//             'key' => 'B8Rs31GE9jOKMJ7W5iXNK0LjpI3IO5Rl'
-//         ]
-//     ]);
-//     $result = json_decode($response->getBody(), true);
-//     $latitude = $result['results'][0]['position']['latitude'];
-//     $longitude = $result['results'][0]['position']['longitude'];
-//     return response()->json([
-//         'latitude' => $latitude,
-//         'longitude' => $longitude
-//     ]);
-// });
+Route::get('/geocode/{address}', function ($address) {
+    $client = new \GuzzleHttp\Client(["verify" => false]);
+    $response = $client->get('https://api.tomtom.com/search/2/geocode/' . urlencode($address) . '.json', [
+        'query' => [
+            'key' => 'B8Rs31GE9jOKMJ7W5iXNK0LjpI3IO5Rl'
+        ]
+    ]);
+    $result = json_decode($response->getBody(), true);
+    $latitude = $result['results'][0]['position']['latitude'];
+    $longitude = $result['results'][0]['position']['longitude'];
+    return response()->json([
+        'latitude' => $latitude,
+        'longitude' => $longitude
+    ]);
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
