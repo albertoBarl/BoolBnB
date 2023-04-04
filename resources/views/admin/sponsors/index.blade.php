7729979a743fae75659d222fc26954b40e1f2fc5
albertoBarl <alberto.barletta00@gmail.com>
2023-04-04 14:19:18 +0200

    optimized code

--- resources/views/admin/sponsors/index.blade.php
+++ resources/views/admin/sponsors/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.admin')
 @section('content')
     <section id="sponsor-page" class="container-fluid p-3">
-        <h3 class="text-uppercase">available sponsor</h3>
+        <h3 class="text-uppercase">available sponsorhips</h3>
         <div class="d-flex align-items-center gap-5">
             @if (session('success_message'))
                 <div class="alert alert-success">
@@ -21,14 +21,6 @@
                 {{-- {{ route('admin.sponsorships.store') }} --}}
                 @csrf
                 <div class="form-group my-3">
-                    <label class="control-label">Select a sponsorship:</label>
-                    <select name="sponsor" id="sponsor">
-                        @foreach ($sponsors as $item)
-                            <option value="{{ $item->id }}">{{ $item->title }}</option>
-                        @endforeach
-                    </select>
-                </div>
-                <div class="form-group my-3">
                     <label class="control-label">Select an apartment:</label>
                     <select name="apartment" id="apartment">
                         @foreach ($apartments as $item)
@@ -38,7 +30,7 @@
                 </div>
                 <div class="form-group my-3">
                     <label class="control-label"></label>
-                    <input type="date" name="date_of_start" value="date_of_start">
+                    <input type="date" name="date_of_start" value="date_of_start" readonly>
                 </div>
                 <div class="form-group my-3">
                     <button type="submit" class="btn btn-sm btn-success">Subscribe</button>
@@ -53,29 +45,47 @@
                     <p class="card-body">Metti in <strong>"evidenza"</strong> il tuo appartamento per {{ $item->duration }}
                         ore a soli <span class="p-1">${{ $item->price }}</span>.
                     </p>
-                    <button class="selectedChoice" value="{{ $item->price }}"
-                        onclick="getPrice({{ $item->price }})">{{ $item->price }}</button>
+                    <button type="button" class="btn btn-light selectedChoice" value="{{ $item->price }}"
+                        onclick="getPrice({{ $item->price }})" data-bs-toggle="modal"
+                        data-bs-target="#exampleModal">${{ $item->price }}</button>
                 </div>
             @endforeach
         </div>
-        <form method="post" id="payment-form" action="{{ url('/checkout') }}">
-            @csrf
-            <section>
-                <label for="amount">
-                    <span class="input-label">Amount</span>
-                    <div class="input-wrapper amount-wrapper">
-                        <input id="amount" name="amount" type="tel" min="1" placeholder="Amount"
-                            value="" disabled>
+        <!-- Modal -->
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="exampleModalLabel">Payment</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
-                </label>
-                <div class="bt-drop-in-wrapper d-flex justify-content-start">
-                    <div id="bt-dropin"></div>
+                    <div class="modal-body">
+                        <form method="post" id="payment-form" action="{{ url('/checkout') }}">
+                            @csrf
+                            <section>
+                                <label for="amount">
+                                    <span class="input-label">Amount</span>
+                                    <div class="input-wrapper amount-wrapper">
+                                        <input id="amount" name="amount" type="tel" min="1"
+                                            placeholder="Amount" value="" readonly>
+                                    </div>
+                                </label>
+                                <div class="bt-drop-in-wrapper d-flex justify-content-start">
+                                    <div id="bt-dropin"></div>
+                                </div>
+                            </section>
+                            <input id="nonce" name="payment_method_nonce" type="hidden" />
+                            <button class="button btn btn-success" type="submit"><span>BUY IT</span></button>
+                        </form>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    </div>
                 </div>
-            </section>
-            <input id="nonce" name="payment_method_nonce" type="hidden" />
-            <button class="button btn btn-success" type="submit"><span>Test
-                    Transaction</span></button>
-        </form>
+            </div>
+        </div>
+
+
     </section>
 
     {{-- payments --}}
