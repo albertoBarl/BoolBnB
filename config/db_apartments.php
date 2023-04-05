7f2b6b500f9dc9de2980fa1306d573ec5c7f57f6
N1colas7 <nicolasnd9897@gmail.com>
2023-04-05 16:16:13 +0200

    fix seeder

--- config/db_apartments.php
+++ config/db_apartments.php
@@ -8,9 +8,9 @@ return
             [
                 "user_id" => "1",
 
-                "title" => "Villino Villa Franca",
+                "title" => "Appartamento Bagnoregio",
 
-                "description" => "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+                "description" => "Ottimo villino nel paesino di Bagnoregio vicino alla meta turistica di Civita!",
 
                 "square_feet" => "150",
 
@@ -20,7 +20,7 @@ return
 
                 "address" => "Via Del Divino Amore, 139 01022 Bagnoregio",
 
-                "image" => "https://images.unsplash.com/photo-1580202313707-46a966af5c6f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1462&q=80",
+                "image" => "https://images.unsplash.com/photo-1588880331179-bc9b93a8cb5e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8dmlsbGlub3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
 
                 "latitude" => "42.63169",
 
