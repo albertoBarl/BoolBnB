bbad0a6ac8512bc79eaf433f0b7a0aebc2fd0cea
N1colas7 <nicolasnd9897@gmail.com>
2023-04-05 15:58:30 +0200

    seeder done

--- config/db_apartments.php
+++ config/db_apartments.php
@@ -18,16 +18,183 @@ return
 
                 "room" => "4",
 
-                "address" => "Via Divino Amore, 139",
+                "address" => "Via Del Divino Amore, 139 01022 Bagnoregio",
 
-                "image" => "https://www.villafrancahotel.it/cms-admin/materiale/pagine/elemento-499-75/villa-franca-hotel.jpg",
+                "image" => "https://images.unsplash.com/photo-1580202313707-46a966af5c6f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1462&q=80",
 
                 "latitude" => "42.63169",
 
                 "longitude" => "12.08030",
 
                 "price" => "150",
-                "free_cancellation" => 0,
-            ]
+            ],
+            [
+                "user_id" => "1",
+
+                "title" => "Appartamento Malpensa",
+
+                "description" => "Questo piccolo appartamento privato a 10 minuti di auto dall'aeroporto di Malpensa tutto per gli ospiti!",
+
+                "square_feet" => "80",
+
+                "bathroom" => "1",
+
+                "room" => "1",
+
+                "address" => "Via Aldo Moro, 6, 21010 Ferno VA",
+
+                "image" => "https://images.unsplash.com/photo-1580202313707-46a966af5c6f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1462&q=80",
+
+                "latitude" => "45.611573",
+
+                "longitude" => "8.758279",
+
+                "price" => "65",
+            ],
+            [
+                "user_id" => "1",
+
+                "title" => "Appartamento Milano Centro",
+
+                "description" => "Silenzioso monolocale su due livelli nel centro storico e commerciale di Milano (piazza Duomo a 500m), ideale per soggiorni di affari o alla scoperta della città.",
+
+                "square_feet" => "50",
+
+                "bathroom" => "1",
+
+                "room" => "1",
+
+                "address" => "Via Pantano, Milano, MI",
+
+                "image" => "https://images.unsplash.com/photo-1502672260266-1c1ef2d93688?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1080&q=80",
+
+                "latitude" => "45.459660",
+
+                "longitude" => "9.191767",
+
+                "price" => "145",
+            ],
+            [
+                "user_id" => "1",
+
+                "title" => "Villa Fiumenaro Siciliana",
+
+                "description" => "Un luogo incantevole in tutte le stagioni :
+                - popolata nel periodo estivo, la lunga e ampia spiaggia sabbiosa offre ampie zone tranquille;
+                - durante la restante parte dell'anno silenziosa, ideale per il relax scandito dal rumore delle onde che si infrangono sulla riva.",
+
+                "square_feet" => "180",
+
+                "bathroom" => "3",
+
+                "room" => "2",
+
+                "address" => "Via Capo Lilibeo, 3",
+
+                "image" => "https://images.unsplash.com/photo-1644012968624-c98b029dfa8b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHZpbGxhJTIwc3VsJTIwbWFyZSUyMGl0YWxpYXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+
+                "latitude" => "37.238396",
+
+                "longitude" => "13.619930",
+
+                "price" => "350",
+            ],
+            [
+                "user_id" => "1",
+
+                "title" => "Appartmento Via Degli Alisei ",
+
+                "description" => "Un perfetto mix di cultura Barocca e il bellissimo mare,gastronomia siciliana e tanto altro.Noi citroviamo in una zona tranquilla e panoramica a 2 km dalla spiaggia ",
+
+                "square_feet" => "180",
+
+                "bathroom" => "3",
+
+                "room" => "2",
+
+                "address" => "Via Delgi Alisei, 38",
+
+                "image" => "https://images.unsplash.com/photo-1459767129954-1b1c1f9b9ace?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fGFwcGFydGFtZW50b3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+
+                "latitude" => "37.237700",
+
+                "longitude" => "13.620661",
+
+                "price" => "350",
+            ],
+            [
+                "user_id" => "1",
+
+                "title" => "Appartamento a Civita di Bagnoregio",
+
+                "description" => "Oltrepassando l'antico portone in legno vi ritroverete in un incantevole luogo dove i tipici caratteri architettonici viterbesi dei tempi passati, possenti muri di tufo, travi in legno a vista, pavimenti in mattoni e particolari in pietra basaltina, si fondono alle comodità dei nostri giorni.
+                Dalla grande terrazza potrete godere della vista sulla Valle dei Calanchi.",
+
+                "square_feet" => "40",
+
+                "bathroom" => "1",
+
+                "room" => "2",
+
+                "address" => "Via S. Maria del Cassero, 31",
+
+                "image" => "https://images.unsplash.com/photo-1610060325112-ebc7efc32a4e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y2l2aXRhfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+
+                "latitude" => "42.627727",
+
+                "longitude" => "12.113317",
+
+                "price" => "200",
+            ],
+            [
+                "user_id" => "1",
+
+                "title" => "Casa Lante",
+
+                "description" => "Lantehouse è un appartamento appena ristrutturato di 35m2 situato al primo piano con ingresso indipendente,che propone uno vista sullo splendido giardino rinascimentale di Villa Lante con ingresso dello stesso a 30m dall'abitazione.",
+
+                "square_feet" => "35",
+
+                "bathroom" => "1",
+
+                "room" => "1",
+
+                "address" => "Via Romanelli, 17 01100 Viterbo VT",
+
+                "image" => "https://images.unsplash.com/photo-1610060301937-9ccfc5cd41ad?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8dml0ZXJib3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+
+                "latitude" => "42.416022",
+
+                "longitude" => "12.105288",
+
+                "price" => "50",
+            ],
+            [
+                "user_id" => "1",
+
+                "title" => "Rifugio Lago Nambino",
+
+                "description" => "Lantehouse è un appartamento appena ristrutturato di 35m2 situato al primo piano con ingresso indipendente,che propone uno vista sullo splendido giardino rinascimentale di Villa Lante con ingresso dello stesso a 30m dall'abitazione.",
+
+                "square_feet" => "70",
+
+                "bathroom" => "1",
+
+                "room" => "2",
+
+                "address" => "Via Adamello, 11, 38086 Madonna di Campiglio TN",
+
+                "image" => "https://images.unsplash.com/photo-1656531103275-d243a64cf6ca?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fG1hZG9ubmElMjBkaSUyMGNhbXBpZ2xpb3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+
+                "latitude" => "46.241581",
+
+                "longitude" => "10.800372",
+
+                "price" => "150",
+            ],
+
         ],
     ];
+
+    
+    
