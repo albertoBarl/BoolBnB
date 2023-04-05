3baf1d2a0a34173ae61e0ec8cae75791a3a82cc5
EsterSPR <web.esterspreafico@gmail.com>
2023-04-05 16:40:44 +0200

    stylized apartments page

--- resources/views/admin/apartments/index.blade.php
+++ resources/views/admin/apartments/index.blade.php
@@ -1,9 +1,9 @@
 @extends('layouts.admin')
 @section('content')
     <div class="d-flex justify-content-between py-4">
-        <div>Appartamenti</div>
+        <div><h2>Appartamenti</h2></div>
         <div>
-            <a href="{{ route('admin.apartments.create') }}" class="btn btn-sm btn-primary">Aggiungi nuovo appartamento</a>
+            <a href="{{ route('admin.apartments.create') }}" class="btn btn-sm btn-outline-dark">Aggiungi nuovo appartamento</a>
         </div>
     </div>
     @if (session('message'))
@@ -13,17 +13,17 @@
     @endif
     <table class="table">
         <thead>
-            <th>id</th>
-            <th>user id</th>
+            <!-- <th>id</th>
+            <th>user id</th> -->
             <th>titolo</th>
-            <th>slug</th>
+            <!-- <th>slug</th> -->
             <th>descrizione</th>
             <th>metri quadrati</th>
             <th>bagni</th>
             <th>camere</th>
             <th>indirizzo</th>
             <th>immagini</th>
-            <th>coordinate</th>
+            <!-- <th>coordinate</th> -->
             <th>prezzo</th>
             <th>cancellazione gratuita</th>
             <th>funzioni</th>
@@ -31,31 +31,31 @@
         <tbody>
             @foreach ($apartments as $item)
                 <tr>
-                    <td>{{ $item->id }}</td>
-                    <td>{{ $item->user_id }}</td>
+                    <!-- <td>{{ $item->id }}</td>
+                    <td>{{ $item->user_id }}</td> -->
                     <td>{{ $item->title }}</td>
-                    <td>{{ $item->slug }}</td>
-                    <td >{{ $item->description }}</td>
+                    <!-- <td>{{ $item->slug }}</td> -->
+                    <td class="text-truncate" style="max-width: 150px">{{ $item->description }}</td>
                     <td>{{ $item->square_feet }}</td>
                     <td>{{ $item->bathroom }}</td>
                     <td>{{ $item->room }}</td>
                     <td>{{ $item->address }}</td>
-                    <td class="d-inline-block text-truncate" style="max-width: 150px">{{ $item->image }}</td>
-                    <td>{{ $item->latitude }} - {{ $item->longitude }}</td>
+                    <td class="text-truncate" style="max-width: 150px"> <img :src="{{ $item->image }}" alt="{{ $item->title }}" class="img-fluid"></td>
+                    <!-- <td>{{ $item->latitude }} - {{ $item->longitude }}</td> -->
                     <td>{{ $item->price }}</td>
                     <td>{{ $item->free_cancellation }}</td>
                     <td>
                         <div class="d-flex">
-                            <a class="btn btn-sm btn-square btn-primary" href="{{ route('admin.apartments.show', $item->slug)}}" title="Visualizza appartamento">
+                            <a class="btn btn-sm btn-square btn-primary mx-1" href="{{ route('admin.apartments.show', $item->slug)}}" title="Visualizza appartamento">
                                 <i class="fas fa-eye"></i>
                             </a>
-                            <a class="btn btn-sm btn-square btn-warning" href="{{ route('admin.apartments.edit', $item->slug)}}" title="Modifica appartamento">
+                            <a class="btn btn-sm btn-square btn-warning mx-1" href="{{ route('admin.apartments.edit', $item->slug)}}" title="Modifica appartamento">
                                 <i class="fas fa-edit"></i>
                             </a>
                             <form method="POST" action="{{ route('admin.apartments.destroy', $item->slug)}}">
                                 @csrf
                                 @method('DELETE')
-                                <button type="submit" class="btn btn-sm btn-square btn-danger confirm-delete-button" data-bs-toggle="modal" data-bs-target="#delete-apartment-modal" data-title="{{$item->title}}">
+                                <button type="submit" class="btn btn-sm btn-square btn-danger confirm-delete-button mx-1" data-bs-toggle="modal" data-bs-target="#delete-apartment-modal" data-title="{{$item->title}}">
                                     <i class="fas fa-trash"></i>
                                 </button>
                             </form>
