e041c08398da128655d6f203c42b343195eafba2
EsterSPR <web.esterspreafico@gmail.com>
2023-04-06 16:08:52 +0200

    corrected message.show stylization

--- resources/views/admin/messages/show.blade.php
+++ resources/views/admin/messages/show.blade.php
@@ -2,13 +2,13 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="col-12 m-3">
-                <div class="d-flex justify-content-between">
-                    <p><strong>Informazioni Messaggio :</strong> {{$message->content}} </p>
-                    <a href="{{ route('admin.messages.index') }}" class="btn btn-primary">Torna all'elenco</a>
-                </div>
+            <div class="col-12 mt-5">
+                <a href="{{ route('admin.messages.index') }}" class="btn btn-primary">Torna all'elenco</a>
             </div>
-            <div class="col-12 m-3">
+            <div class="col-md-7 col-sm-12 mt-5 my_justify">
+                    <p><strong>Informazioni Messaggio:</strong> {{$message->content}} </p>
+            </div>
+            <div class="col-12 mt-5">
                  <p><strong>Name:</strong> {{$message->name}}</p>
                  <p><strong>Surname:</strong> {{$message->surname}}</p>
                  <p><strong>Email:</strong> {{$message->email}}</p>
