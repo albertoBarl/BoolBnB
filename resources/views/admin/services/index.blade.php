2337d0292244a807619754eab41b75b14b23ec7b
EsterSPR <web.esterspreafico@gmail.com>
2023-04-06 17:35:19 +0200

    responsive apt table + added margin to dashboard

--- resources/views/admin/services/index.blade.php
+++ resources/views/admin/services/index.blade.php
@@ -1,13 +1,13 @@
 @extends('layouts.admin')
 @section('content')
 
-<h2 class="my-4">
+<h2 class="my-4 mx-3 mt-5">
     {{ __('Servizi') }}
 </h2>
 
 @foreach ($services as $item)
 
-<div class="m-2">
+<div class="m-3">
     <li class="my_servicesli">
         {{$item->name}}
     </li>
