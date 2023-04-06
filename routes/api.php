0b9881b49053c0382b18799db6bc962f9de6de65
albertoBarl <alberto.barletta00@gmail.com>
2023-04-06 23:53:27 +0200

    removed comment

--- routes/api.php
+++ routes/api.php
@@ -28,19 +28,3 @@ Route::get('/services', [ServiceController::class, 'index']);
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-// Route::get('/geocode/{address}', function ($address) {
-//     $client = new \GuzzleHttp\Client(["verify" => false]);
-//     $response = $client->get('https://api.tomtom.com/search/2/geocode/' . urlencode($address) . '.json', [
-//         'query' => [
-//             'key' => 'B8Rs31GE9jOKMJ7W5iXNK0LjpI3IO5Rl'
-//         ]
-//     ]);
-//     $result = json_decode($response->getBody(), true);
-//     $latitude = $result['results'][0]['position']['lat'];
-//     $longitude = $result['results'][0]['position']['lon'];
-//     return response()->json([
-//         'latitude' => $latitude,
-//         'longitude' => $longitude
-//     ]);
-// });
