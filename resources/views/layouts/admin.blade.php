2bce21d6f5ea235df5e8a494eee8b016918850db
EsterSPR <web.esterspreafico@gmail.com>
2023-04-06 16:41:20 +0200

    added page title + removed image from apt.index

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -8,7 +8,8 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <!-- <title>{{ config('app.name', 'Laravel') }}</title> -->
+    <title>BoolBnB - Business</title>
 
 
     <!-- Fonts -->
