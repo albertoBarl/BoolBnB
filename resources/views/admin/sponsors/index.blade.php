4d5cb345cf53d549e06fe3ee91d13cff11dabf1b
EsterSPR <web.esterspreafico@gmail.com>
2023-04-07 12:37:15 +0200

    stylized sponsor cards

--- resources/views/admin/sponsors/index.blade.php
+++ resources/views/admin/sponsors/index.blade.php
@@ -1,34 +1,36 @@
 @extends('layouts.admin')
 @section('content')
-    <section id="sponsor-page" class="container-fluid p-3">
-        <h3 class="text-uppercase">available sponsorhips</h3>
+    <section id="sponsor-page" class="container-fluid pt-5">
+        
+        <h2>Sponsors</h2>
+        <p>Un appartamento sponsorizzato apparirà in alto nell'home page.</p>
+
         @if (session('success_message'))
             <div class="alert alert-success">
                 {{ session('success_message') }}
             </div>
         @endif
-        {{-- cards --}}
-        <div class="row gap-2 gap-lg-5">
-            @foreach ($sponsors as $sponsor)
-                <div class="col-12 col-lg-3 card p-2 d-flex justify-content-center align-items-center
-                ">
-                    <h5 class="card-title text-center text-uppercase bg-light form-control">
-                        {{ $sponsor->title }}</h5>
-                    <p class="card-body"><strong>"Highlights"</strong> your apartment for
-                        <strong>{{ $sponsor->duration }}</strong>
-                        hours for only <strong>${{ $sponsor->price }}</strong>.
-                    </p>
 
-                    <form action="{{ route('admin.sponsors.show', ['id' => $sponsor->id]) }}" method="get"
-                        class="d-flex flex-column justify-content-center align-items-center">
-                        <label for="apartment_id">Select an apartment:</label>
-                        <select class="form-select mb-2" name="apSlug" id="apartment_id">
+        {{-- cards --}}
+        <div class="row mt-5 ms-1">
+            @foreach ($sponsors as $sponsor)
+                <div class="col-sm-12 col-md-12 col-lg-5 col-xl-3 card me-lg-3 mb-3 p-3 d-flex justify-content-center align-items-center my_sponsorcard">
+
+                    <h5 class="card-title my_sponsortitle text-center text-uppercase bg-light form-control"><strong>{{ $sponsor->title }}</strong></h5>
+                    <p class="card-body text-center mb-0"><strong>Metti in evidenza</strong> il tuo appartamento per <strong>{{ $sponsor->duration }} ore</strong> a soli <strong>{{ $sponsor->price }}&euro;</strong>.</p>
+
+                    <form action="{{ route('admin.sponsors.show', ['id' => $sponsor->id]) }}" method="get" class="d-flex flex-column justify-content-center align-items-center">
+
+                        <label for="apartment_id" class="mb-3">Seleziona un appartamento:</label>
+
+                        <select class="form-select mb-2 border-0" name="apSlug" id="apartment_id">
                             @foreach ($apartments as $apartment)
                                 <option value="{{ $apartment->slug }}">{{ $apartment->title }}</option>
                             @endforeach
                         </select>
+
                         <input type="hidden" name="sponsor_id" value="{{ $sponsor->id }}">
-                        <button type="submit" class="mybtn btn btn-light">Submit</button>
+                        <button type="submit" class="mybtn btn btn-outline-light mt-3">Compra ora</button>
                     </form>
                 </div>
             @endforeach
